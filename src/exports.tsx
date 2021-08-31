22ce8f506d7353ac125f806eb004b9a5c3ad5f62
Vishal Chiluveri <vishalchiluveri@gmail.com>
2021-09-01 00:27:51 +0530

    updated exports

--- src/exports.tsx
+++ src/exports.tsx
@@ -1,4 +1,6 @@
 import { TextEditor } from "./components/TextEditor";
-
+import { marked } from "./utils/marked/marked";
+import { videoRenderer } from "./utils/marked/videoRenderer";
 export * from "./types";
-export { TextEditor };
+
+export { TextEditor, marked, videoRenderer };
